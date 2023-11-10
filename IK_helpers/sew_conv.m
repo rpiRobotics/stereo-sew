0416ba31326a46f835d11461bb5829cc3083720f
Alex Elias <aelias36@yahoo.com>
2023-11-10 15:10:43 -0500

    Update sew_conv.m

--- IK_helpers/sew_conv.m
+++ IK_helpers/sew_conv.m
@@ -1,52 +1,56 @@
 classdef sew_conv
+    % Conventional SEW angle
+
     properties
-        V
+        e_r % Reference unit vector
     end
     
     methods
-        function obj = sew_conv(V)
-            obj.V = V;
+        function obj = sew_conv(e_r)
+            obj.e_r = e_r;
         end
         
         function psi = fwd_kin(obj, S, E, W)
+            % Given shoulder, elbow, and wrist postitions,
+            % Find SEW angle psi
+
+            p_SE = E - S;
+            e_SW = vec_normalize(W - S);
+
+            psi = subproblem.sp_1(obj.e_r, p_SE, e_SW);
+        end
+
+        function [e_CE, n_SEW] = inv_kin(obj, S, W, psi)
+            % Given shoulder and wrist positions, and given SEW angle psi
+            % Find e_CE, unit vector which points towards elbow,
+            % and find n_SEW, which is normal to SEW plane
+
+            e_SW = vec_normalize(W - S);
+            e_y = vec_normalize(cross(e_SW, obj.e_r));
+
+            n_SEW = rot(e_SW, psi) * e_y;
+            e_CE = cross(n_SEW, e_SW);
+        end
+
+        function [J_psi_E, J_psi_W] = jacobian(obj, S, E, W)
+            % Given shoulder, elbow, and wrist positions,
+            % Find Jacobians of SEW angle w.r.t. elbow and wrist velocities
+            
             p_SE = E - S;
             p_SW = W - S;
-            w_hat = vec_normalize(p_SW);
+            e_SW = vec_normalize(p_SW);
+            k_y = cross(p_SW, obj.e_r);
+            e_y = vec_normalize(k_y);
             
-            p = (eye(3) - w_hat*w_hat')*p_SE;
-            
-            psi = atan2( ...
-                    w_hat'*cross(obj.V, p), ...
-                    obj.V'*p);
-        end
+            p_CE = -cross(e_SW, cross(e_SW, p_SE));
+            e_CE = vec_normalize(p_CE);
 
-        function [k, n] = inv_kin(obj, S, W, psi)
-            W_hat = vec_normalize(W-S);
-            y_c = cross(W_hat, obj.V);
-            y_c = y_c / norm(y_c);
-            x_c = cross(y_c, W_hat);
-
-            k = x_c * cos(psi) + y_c * sin(psi);
-            n = cross(W_hat, k);
-        end
-
-        function [J_e, J_w] = jacobian(obj, S, E, W)
-            E = E - S;
-            W = W - S;
-            w_hat = vec_normalize(W);
+            J_psi_E = cross(e_SW, e_CE)' / norm(p_CE);
             
-            ell = cross( cross(W, obj.V) , W);
-            ell_hat = vec_normalize(ell);
+            J_w_1 = dot(e_SW, obj.e_r)/ norm(k_y) * e_y';
+            J_w_2 = dot(e_SW, p_SE)/norm(p_SW)/norm(p_CE) * cross(e_SW, e_CE)';
             
-            p = (eye(3) - w_hat*w_hat')*E;
-            p_hat = vec_normalize(p);
-            
-            J_e = cross(w_hat, p_hat)' / norm(p);
-            
-            J_w_1 = (obj.V'*W) /norm(ell) * cross(w_hat, ell_hat)';
-            J_w_2 = (w_hat'*E)/norm(W)/norm(p) *  cross(w_hat, p_hat)';
-            
-            J_w = J_w_1 - J_w_2;
+            J_psi_W = J_w_1 - J_w_2;
         end
     end
 end
